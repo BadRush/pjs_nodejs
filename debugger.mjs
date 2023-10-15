d45c6781217211e3a60c923420a31513f9d11c09
anamBadrus <anamb4drus@gmail.com>
2023-10-15 18:51:37 +0700

    events

--- debugger.mjs
+++ debugger.mjs
@@ -1,5 +1,5 @@
 function sayHello(name){
-    debugger;
+    debugger;  // disini 
     return `Hello ${name}`;
 }
 
---
+++ events.mjs
@@ -0,0 +1,14 @@
+import { EventEmitter } from "events";
+
+const emitter = new EventEmitter();
+
+emitter.addListener("hello", (name) => {
+    console.info(`hello ${name}`);
+})
+emitter.addListener("hello", (name) => {
+    console.info(`halo ${name}`);
+})
+
+
+
+emitter.emit("hello", "badrus")
