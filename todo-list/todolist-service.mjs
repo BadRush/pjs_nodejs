694bb36133c93da56b5adca8b2656a8fab0b5146
anamBadrus <anamb4drus@gmail.com>
2023-10-16 12:28:24 +0700

    bag-4

---
+++ todo-list/todolist-service.mjs
@@ -0,0 +1,121 @@
+
+
+// export class TodoListService {
+// //api write todolist
+//     todolist = ["anam", "badrus", "salam", "wkwkw"];
+
+//     getJsonTodolist(){
+//         return JSON.stringify({
+//             code: 200,
+//             status: "ok",
+//             data: this.todolist.map((value, index) => {
+//                 return {
+//                     id: index,
+//                     todo: value
+//                 }
+//             })
+//         })
+//     }
+
+//     getTodoList(request,response) {
+//         response.write(this.getJsonTodolist());
+//         response.end();
+//     }
+
+// //api create todolist
+
+//     createTodo(request, response){
+//         request.addListener("data", (data) => {
+//             const body = JSON.parse(data.toString());
+//             this.todolist.push(body.todo);
+
+//             response.write(this.getJsonTodolist());
+//             response.end();
+//         })
+
+//     }
+
+//     //update todo
+//     updateTodo(request, response){
+//         request.addListener("data", (data) => {
+//             const body = JSON.parse(data.toString());
+//             if(this.todolist[body.id]) {
+//                 this.todolist[body.id] = body.todo;
+//             }
+
+//             response.write(this.getJsonTodolist());
+//             response.end();
+//         })
+//     }
+
+//     deleteTodo(request, response){
+//         request.addListener("data", (data) => {
+//             const body = JSON.parse(data.toString());
+//             if (this.todolist[body.id]) {
+//                 this.todolist.splice(body.id, 1);
+//             }
+
+//             response.write(this.getJsonTodoList());
+//             response.end();
+//         })
+//     }
+
+
+// }
+export class TodolistService {
+
+    todolist = ["Programmer", "Zaman", "Now"];
+
+    getJsonTodoList() {
+        return JSON.stringify({
+            code: 200,
+            status: "OK",
+            data: this.todolist.map((value, index) => {
+                return {
+                    id: index,
+                    todo: value
+                }
+            })
+        });
+    }
+
+    getTodoList(request, response) {
+        response.write(this.getJsonTodoList());
+        response.end();
+    }
+
+    createTodo(request, response) {
+        request.addListener("data", (data) => {
+            const body = JSON.parse(data.toString());
+            this.todolist.push(body.todo);
+
+            response.write(this.getJsonTodoList());
+            response.end();
+        })
+    }
+
+    updateTodo(request, response) {
+        request.addListener("data", (data) => {
+            const body = JSON.parse(data.toString());
+            if (this.todolist[body.id]) {
+                this.todolist[body.id] = body.todo;
+            }
+
+            response.write(this.getJsonTodoList());
+            response.end();
+        })
+    }
+
+    deleteTodo(request, response){
+        request.addListener("data", (data) => {
+            const body = JSON.parse(data.toString());
+            if (this.todolist[body.id]) {
+                this.todolist.splice(body.id, 1);
+            }
+
+            response.write(this.getJsonTodoList());
+            response.end();
+        })
+    }
+
+}
